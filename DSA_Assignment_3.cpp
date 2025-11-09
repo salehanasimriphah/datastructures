d9e7d6af1edd72b115078ccf1ac13f9417b01297
EmaanAhmed05 <emaanahmed662@gmail.com>
2025-11-09 07:58:12 -0800

    Add files via upload
    updated file

--- DSA_Assignment_3.cpp
+++ DSA_Assignment_3.cpp
@@ -40,29 +40,54 @@ void postfix(Node* root)
 	}
 }
 int main(){
+	int choice;
 	Node* root = createnode('*');
 	root->left = createnode('+');
 	root->right = createnode('-');
 	
-	root->left->left = createnode('5');
-	root->left->right = createnode('4');
+	root->left->left = createnode('A');
+	root->left->right = createnode('B');
 	
-	root->right->left = createnode('3');
-	root->right->right = createnode('2');
+	root->right->left = createnode('C');
+	root->right->right = createnode('D');
+do{
+	cout<<"\n_______Menu_______\n";
+	cout<<"\n 1)Infix_Expression\n";
+	cout<<"\n 2)Prefix_Expression\n";
+	cout<<"\n 3)Postfix_Expression\n";
+	cout<<"\n 4)Exit \n";
+	cout<<"\n Enter your choice: \n";
+	cin>>choice;
 
+switch(choice){
 
+case 1:
 	cout<<"Infix Expression ";
 	infix(root);
 	cout<<endl;
+	break;
 	
+	case 2:
 	cout<<"Prefix Expression ";
 	prefix(root);
 	cout<<endl;
+	break;
 	
+	case 3:
 	cout<<"Postfix Expression ";
 	postfix(root);
 	cout<<endl;
+	break;
 	
+	case 4:
+		cout<<"Exit Program_!!! \n";
+		break;
+			
+		default:
+			cout<<"Invalid Choice_!!! \n";
+}}
+while(choice != 4);
+
 	return 0;
 	
 }
